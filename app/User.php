0a3256ea33b92cde26f04935c2d39a684c45c8b3
Timofey Krischenko <tima.krischenko@gmail.com>
2020-08-02 09:34:40 +0700

    настройка авторизации

--- app/User.php
+++ app/User.php
@@ -5,10 +5,10 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
-{
-    use Notifiable;
+class User extends Authenticatable implements MustVerifyEmail {
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -26,14 +26,5 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
